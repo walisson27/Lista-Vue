86ed8186235c3a3bfeb0136f102f0eafab2d11a1
unknown <walisson_souza7@hotmail.com>
2021-10-11 23:05:01 -0300

    to-do list

---
+++ dist/precache-manifest.3b0b3ca271cfda223b461c6518c8634d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2b744686812e84ec06c",
+    "url": "/css/chunk-6f081a75.2102bf8a.css"
+  },
+  {
+    "revision": "d69ee1a8d9992895c819",
+    "url": "/css/chunk-bcb1f2d2.15290f63.css"
+  },
+  {
+    "revision": "5299940cd07061be4662",
+    "url": "/css/chunk-vendors.f18fbbea.css"
+  },
+  {
+    "revision": "0f1bc6ad9ff9d22b2135f634c006a033",
+    "url": "/img/sunset.0f1bc6ad.jpg"
+  },
+  {
+    "revision": "d31c5fe4db937de31a5fc42a4b0b62bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1662057ab524a3f70cb",
+    "url": "/js/app.4ed5d853.js"
+  },
+  {
+    "revision": "f2b744686812e84ec06c",
+    "url": "/js/chunk-6f081a75.884b76a7.js"
+  },
+  {
+    "revision": "d69ee1a8d9992895c819",
+    "url": "/js/chunk-bcb1f2d2.ec6cf499.js"
+  },
+  {
+    "revision": "5299940cd07061be4662",
+    "url": "/js/chunk-vendors.698d9720.js"
+  },
+  {
+    "revision": "eb7d38325433b7428cf3cd9ae7236e3e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ src/views/About.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="pa-3">
+    <h1>Sobre</h1>
+      <p>
+        Essa é uma lista de tarefas desenvolvida com Vue.js. 
+      </p>
+       
+      <p>
+        Algumas ferramentas utilizadas aqui foram:
+      </p>
+      <v-list>
+      <template v-for="(item) in tacnologias">
+
+        <v-list-item
+          :key="item.titulo"
+        >
+          <v-list-item-avatar>
+            <v-icon>{{item.icon}}</v-icon>
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title v-html="item.titulo"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+    </v-list>
+    <hr>
+    <section>
+      <p>
+        <v-card-subtitle >Contato:</v-card-subtitle>
+        <v-card-text>
+        <strong>📨E-mail:</strong>walisson_souza7@hotmail.com<br>
+        <strong>🧑‍💻 Walisson Souza</strong><br>
+        <img src="../assets/octocat.png" height="20px" alt="imagem octocat" /> <a href="https://github.com/walisson27">/WalissonSouza</a>
+        </v-card-text>
+      </p>
+    </section>
+      
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return{
+      tacnologias:[
+        {
+          titulo: "HTML",
+          icon: "mdi-language-html5"
+        },
+        {
+          titulo: "Vue.js",
+          icon: "mdi-vuejs"
+        },
+        {
+          titulo: "Vuetify",
+          icon: "mdi-vuetify"
+        },
+        {
+          titulo: "VUEX",
+          icon: "mdi-vuejs"
+        },
+        {
+          titulo: "VueRouter",
+          icon: "mdi-vuejs"
+        },
+        {
+          titulo: "Localbase DB",
+          icon: "mdi-database"
+        },
+        {
+          titulo: "Animate.css",
+          icon: "mdi-transition"
+        },
+        {
+          titulo: "Drag and Drop",
+          icon: "mdi-drag-variant"
+        },
+        {
+          titulo: "Git",
+          icon: "mdi-git"
+        },
+        {
+          titulo: "GitHub",
+          icon: "mdi-github"
+        },
+      ]
+    }
+  }
+}
+</script>
+
